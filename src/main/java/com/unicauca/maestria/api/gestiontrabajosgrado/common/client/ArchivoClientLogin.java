72898b9b018c39a8dd7c598d2982438508a8c851
juliomellizo <117930717+juliomellizo@users.noreply.github.com>
2024-08-14 23:30:34 -0500

    Update 10.0
    Solucionado problema de client

--- src/main/java/com/unicauca/maestria/api/gestiontrabajosgrado/common/client/ArchivoClientLogin.java
+++ src/main/java/com/unicauca/maestria/api/gestiontrabajosgrado/common/client/ArchivoClientLogin.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-gestion-login", url = "http://localhost:8080")
+@FeignClient(name = "ms-gestion-login", url = "http://ms-gestion-login:8080")
 public interface ArchivoClientLogin {
 
     @GetMapping("/api/auth/obtenerCorreo/{user}")
